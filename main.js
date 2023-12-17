e68ed24f5f0bc251d36365145fd1912ae8debcbc
kenanmusali <textme0306@gmail.com>
2023-12-17 11:54:01 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,165 @@
+// task-01
+// console.log("my name is Tom");
+ 
+// task-02//
+// let name = "Tommy";
+// console.log(name);
+ 
+// task-03
+// 2user, person, console, $add
+ 
+// task-04
+// let bookPrice = 750;
+// let bookAmount = 14;
+// let totalValue = bookPrice * bookAmount;
+// console.log("The total value of all books: " + totalValue + "AZN");
+ 
+// task-05
+// let kmhSpeed = 75;
+// const conversionFactor = 1 / 3.6;
+// const msSpeed = kmhSpeed * conversionFactor;
+// console.log("(m/s): " + msSpeed);
+
+// task-06
+// let year = Number (prompt ("Enter your Birth year: "));
+// let age = 2023 - parseInt(year);
+// console.log("(age): " + age * 365);
+
+// task-07
+// let num = 2;
+// let FifthPower = Math.pow(num, 5);
+// let dividedBy3 = num / 3;
+// console.log("Number raised to the fifth power: " + FifthPower);
+// console.log("The number divided by 3: " + dividedBy3);
+
+// task-08
+// let firstName = "Alan";
+// let lastName = "Turing";
+// let result = "Sizin adınız " + firstName + " " + lastName + " Tanışlığımıza çox şadam!";
+// console.log(result);
+ 
+// task-09
+// let firstName = prompt ("Enter your Name: ");
+// let lastName = prompt ("Enter your Last Name: ");
+// let promptMessage = "Sizi bir daha görməyimizə şadıq, ";
+// let result = promptMessage + firstName + " " + lastName;
+// console.log(result);
+ 
+// task-10
+// let fullName = "Murad Orucov"
+// let letterCount = fullName.replace(/ /g, '').length;
+// console.log("Number of letters in the name: " + letterCount);
+ 
+// task-11
+// let fullName = prompt("Enter your full name: ");
+// let names = fullName.split();
+// let firstNameInitial = names[0].charAt(0);
+// let lastNameInitial = names[names.length - 1].charAt(0);
+// console.log("Letters: " + firstNameInitial + lastNameInitial);
+ 
+// task-12
+// let firstName = prompt ("Enter your Name: ");
+// let lastName = prompt ("Enter your Last Name: ");
+// let result = lastName + " " + firstName;
+// console.log(result);
+ 
+// task-13
+// let firstName = prompt ("Enter your Name: ");
+// let result = ("Salam, ") + firstName;
+// console.log(result);
+
+// task-14
+// let number1 = prompt("Enter the first number: ");
+// let number2 = prompt("Enter the second number: ");
+// strings to numbers
+// let parsedNumber1 = parseFloat(number1);
+// let parsedNumber2 = parseFloat(number2);
+// Addition
+// let addition = parsedNumber1 + parsedNumber2
+// console.log(addition);
+// Subtraction
+// let subtraction = parsedNumber1 - parsedNumber2
+// console.log(subtraction);
+//  Multiplication
+// let multiplication = parsedNumber1 * parsedNumber2
+// console.log(multiplication);
+//  Division
+// let division = parsedNumber1 / parsedNumber2
+// console.log(division);
+
+// task-15
+// let num1 = 12345
+// let num2 = 98765
+// let result = (num1 - num2) / 6
+// console.log(result);
+
+// task-16
+// let result = "" + 1 + 0; console.log(result);
+// let result = "" - 1 + 0; console.log(result);
+// let result = true - false; console.log(result);
+// let result = 6 / "3"; console.log(result);
+// let result = 4 + 5 + "px"; console.log(result);
+// let result = "$" + 4 + 5; console.log(result);
+// let result = "4" - 2; console.log(result);
+// let result = "4px" - 2 + "px"; console.log(result);
+// let result = 7 / 0; console.log(result);
+// let result = "-9" + 5; console.log(result);
+// let result = "-9" - 5; console.log(result);
+// let result = null + 1; console.log(result);
+// let result = undefined + 1; console.log(result);
+
+// task-17
+// let a = +prompt("Enter the number: ")
+// let b = +prompt("Enter the number: ")
+// let c = +prompt("Enter the number: ")
+// let d = +prompt("Enter the number: ")
+// console.log(a + b + c + d)
+
+// task-18
+// let fullName = prompt("Enter your full name")
+// let result = ("Salam " + fullName + "!")
+// console.log(result);
+
+// task-19
+// let income = +prompt("In Come Money >> ")
+// let left = 530;
+// let right = 90;
+// let a = parseInt(income)
+// let b = l(income.tofixed(2))
+// console.log(b + " manat")
+
+// task-20
+//
+
+//task-21
+//
+
+//task-22
+//
+
+//task-23
+//
+
+//task-24
+//
+
+//task-25
+//
+
+//task-26
+//
+
+//task-27
+//
+
+//task-28
+//
+
+//task-29
+//
+
+//task-30
+//
+
+
+
