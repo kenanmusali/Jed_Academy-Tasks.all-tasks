899f286c3986d306c03d842a7f88d02aba2131ac
kenanmusali <textme0306@gmail.com>
2023-12-28 09:42:52 -0800

    upgrated v3.1

---
+++ Task 28/main.js
@@ -0,0 +1,169 @@
+// let balance = 1000;
+// balance -= 500;
+// console.log(balance);
+// balance -= 10;
+// console.log(balance);
+// balance -= 200;
+// console.log(balance);
+
+// let balance = 1000;
+// function getBalance() {
+//     console.log(balance);
+// }
+// balance -= 500;
+// getBalance()
+// balance -= 10;
+// getBalance()
+// balance -= 200;
+// getBalance()
+
+// let startVal = 0;
+// let endValue = 20;
+// startVal += 1;
+// console.log(startVal);
+// startVal += 1;
+// console.log(startVal);
+// startVal += 1;
+// console.log(startVal);
+
+// let startVal = 0;
+// let endValue = 20;
+// function customLoop() {
+//     startVal += 1;
+//     console.log(startVal);
+// }
+// customLoop()
+// customLoop()
+// customLoop()
+
+// let startVal = 0;
+// let endValue = 20;
+// function customLoop() {
+//     startVal += 1;
+//     console.log(startVal);
+//     customLoop()
+// }
+// customLoop()
+
+// let startVal = 0;
+// let endValue = 100;
+// function customLoop() {
+//     if (startVal < endValue) {
+//         startVal += 1;
+//         console.log(startVal);
+//         customLoop()
+//     }
+// }
+// customLoop()
+
+// let startVal = 0;
+// let endValue = 100;
+// function customLoop() {
+//     if (startVal < endValue) {
+//         startVal += 1;
+//         if (startVal % 2 === 0) {
+//             console.log(startVal);
+//         }
+//         customLoop()
+//     }
+// }
+// customLoop()
+
+// let startVal = 0;
+// let endValue = 100;
+// function customLoop() {
+//     if (startVal < endValue) {
+//         startVal += 1;
+//         if (startVal % 2 === 0) {
+//             console.log(startVal);
+//         }
+//         customLoop()
+//     }
+// }
+// customLoop()
+
+// let startVal = 0;
+// const endValue = 100;
+// while (startVal < endValue) {
+//     startVal += 1;
+//     if (startVal % 2 === 0) {
+//         console.log(startVal);
+//     }
+// }
+
+// let start = 0
+// let end = 100
+// while (start < end) {
+//     start++;
+//     console.log(start);
+// }
+
+// let val = "hello world"
+// let i = 0
+// while (i < val.length) {
+//     console.log(i)
+//     i++;
+// }
+
+// let val = "hello world";
+// let i = 0;
+// while (i < val.length) {
+//     console.log(val[i])
+//     i++;
+// }
+
+// let val = "hello world";
+// let i = val.length;
+// while (i >= 0) {
+//     console.log(val[i]);
+//     i--;
+// }
+
+// let val = "hello world";
+// let result = ""
+// let i = val.length -1;
+// while (i >= 0) {
+//     // result = result +val[i]
+//     result += val[i]
+//     i--;
+// }
+// console.log(result);
+
+// while (true) {
+//     let val = +prompt("5 + 5 = how much will be?");
+//     if (val === 10) {
+//         alert("Correct Answer")
+//         break;
+//     } 
+//     else {
+//         alert("Wrong Answer")
+//         continue;
+//     }
+// }
+
+// let i = 0
+// while (i < 10) {
+//     console.log(i);
+//     i++
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// function printNumbers(i) {
+//     if (i < 10) {
+//         console.log(i);
+//         printNumbers(i + 1); 
+//     }
+// }
+// printNumbers(0);
+
+// function printNumbers(i) {
+//     if (i < 10) {
+//         console.log(i);
+//         i++;
+//         printNumbers(i);
+//     }
+// }
+// printNumbers(0);
