1df14364a7c5352aef34ac1df6452f3572440174
kenanmusali <textme0306@gmail.com>
2023-12-28 12:38:32 -0800

    upgrated v4.1

--- Javascript/Task 28/main.js
+++ Javascript/Task 28/main.js
@@ -168,6 +168,60 @@
 // }
 // printNumbers(0);
 
+// let number = 20;
+// let num = number;
+// let i = 0;
+// while (i < num) {
+//     if (10 % i === 0 && i !==1 && i !== number) {
+//         console.log(i)
+//     }
+//     i++;
+// }
+
+// let n = 42;
+// let i = n;
+// while (i >= 1) {
+//     if (i !== 1 && prime(i)) {
+//         console.log(i);
+//     }
+//     i--;
+// }
+// function prime(num) {
+//     let i = 2;
+//     while (i <= num ** 0.5) {
+//         if (num % i === 0) {
+//             return false;
+//         }
+//         i++;
+//     }
+//     return true;
+// }
+
+
+
+
+
+
+// let num = 15;
+
+// while (num > 1) {
+
+//     let i = num;
+//     let count = 0;
+//     while (i > 0) {
+//         if (num % i === 0) {
+//             count += 1;
+//         }
+//         i--;
+//     }
+//     if (count === 2) {
+//         console.log(num, "sadedir");
+//     }
+//     num--;
+// }
+
+
+
 // Task-1
 // Task-2
 // Task-3
