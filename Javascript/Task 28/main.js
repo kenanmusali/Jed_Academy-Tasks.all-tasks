4793b90e8cbd76e2bfe01799cf5ee5600391465e
kenanmusali <textme0306@gmail.com>
2024-01-05 01:30:24 -0800

    v4.2.1

--- Javascript/Task 28/main.js
+++ Javascript/Task 28/main.js
@@ -57,14 +57,59 @@
 //     num++;
 // }
 
+// let num = 100;
+// while (num < 991) {
+//         console.log(num)
+//     num+= 10;
+// }
+
 // Task-4
 //Bütün ikirəqəmli tək ədədlərin cəmini konsola çıxaran proqram yazın.
 
+
+
 // Task-5
-//
+//İstifadəçinin daxil etdiyi ədədin bölünəni olan bütün üçrəqəmli ədədlərin cəmini hesablayan proqram yazın.
+
+// let num = 10;
+// let i = 100;
+// let sum = 0;
+// while (i < 1000) {
+//     if (i % num === 0) {
+//         sum += i
+//         console.log(sum);
+//     }
+//     i++;
+// }
+
+// let num = 10;
+// let i = 100;
+// while (i < 1000) {
+//     if (i % num === 0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
 
 // Task-6
+//Sadə ədədləri çıxaran proqram yazın. İstifadəçidən n ədədini soruşan və n-dən 0-a kimi bütün sadə ədədləri azalan ardıcıllıqla çıxaran proqram yazın. «23» ədədi daxil edildikdə, konsola «23», «19», «17», «13», «11», «7», «5», «3», «2», «1» ardıcıllığı çıxarılmalıdır.
+
+
+
 // Task-7
+//10-a qədər bütün reqemlerin hasilin tapan proqram tərtib edin Alınan dəyəri konsola çıxarın.
+
+
+
 // Task-8
+//İstifadəçidən mətn daxil etməsini tələb edən və mətn elementlərini əks qaydada düzən proqram tərtib edin.
+
+
+
 // Task-9
-// Task-10
+//Mətndə "." hərfinin ilk dəfə hansı indeksdə yerləşdiyini tapan proqram tərtib edin.
+
+
+
+// Task-10 (isNaN)
+//Mətndə rəqəmlərin olub olmadığını yoxlayan proqram yazın. İstifadəçidən mətn daxil etməsini tələb edən və mətndə rəqəmlərin olub-olmaması barədə konsola mesaj çıxaran proqram tərtib edin
