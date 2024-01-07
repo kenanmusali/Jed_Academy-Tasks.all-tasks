42a5dc3b63a4d4dc23b6dd32a9737d76283bd5bf
kenanmusali <textme0306@gmail.com>
2024-01-07 11:55:57 -0800

    4.2

---
+++ Javascript/Task 29/main.js
@@ -0,0 +1,13 @@
+// Task-01
+// İstifadəçi adını parametr kimi qəbul edən funksiya yazın. İstifadəçi adını parametr kimi qəbul edən və konsola «[İstifadəçi adı] is active now» mesajı çıxaran funksiya tərtib edin. Funksiyanı 3 müxtəlif ad üçün çağırın.
+
+let name = "Tom Cruise";
+let result = name;
+function user() {
+    result = result + " is active now";
+}
+user();
+console.log(result);
+
+// Task-01
+// 
