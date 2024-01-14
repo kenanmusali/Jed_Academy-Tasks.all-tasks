cd2c6705e013b58bea98a499e5066a4500869e94
kenanmusali <textme0306@gmail.com>
2024-01-14 07:55:14 -0800

    4.3

--- Javascript/Task 31/main.js
+++ Javascript/Task 31/main.js
@@ -1,38 +1,79 @@
+//Task-01
+//HTML-kod verilmişdir. Siyahının ilk elementini konsola çıxarın.
 
-// console.log(document.querySelector("h1"));
-// const styleElement = document.querySelector(".menu li:first-child")
-// const styleElement1 = document.querySelector(".menu li:nth-child(2)")
-// const styleElement2 = document.querySelector(".menu li:nth-child(3)")
-// styleElement1.style.backgroundColor = "gray"
-// styleElement.style.backgroundColor = "red"
-// console.log(styleElement);
-// console.log(styleElement1);
-// console.log(styleElement2);
+// const styleElement = document.querySelector("li:first-child");
+// console.log(styleElement.innerHTML);
+// console.log(styleElement.innerText);
+// console.log(styleElement.textContent);
 
-// let h2elem = document.querySelector(".menu");
-// let pElem = document.createElement("h1");
-// pElem.innerText = "4";
-// // h2elem.append(pElem);
-// h2elem.insertAdjacentHTML(pElem, secondItem);
-// let secondItem = document.querySelector("li:nth-child(2)")
-// console.log(h2elem)
+//Task-02
+//HTML-kod verilmişdir. Yalnız JS-kodundan istifadə edərək dəyişklik edin:
+//- Başlığı silin;
+//- Siyahının sonuna mətn ilə yeni bir bölmə «TITAN RTX» əlavə edin .
 
-// let h2elem = document.querySelector(".menu");
-// let pElem = document.createElement("h1");
-// pElem.innerText = "4";
-// h2elem.insertAdjacentElement("afterbegin", pElem);
-// console.log(h2elem);
+// let styleElement = document.querySelector("h1");
+// styleElement.remove();
+// let newElement = document.createElement("h1");
+// newElement.innerText = "TITAN RTX";
+//
+//document.body.insertAdjacentElement("afterend", newElement);
+// document.body.append(newElement);
+
+//Task-03
+//HTML-kod verilmişdir. Yalnız JS-kodundan istifadə edərək dəyişklik edin:
+//- başlıq mətnini «GeForce 20 Series» -a;
+//- fon rəngi stili primary klası olan elementləri #ebebeb rənginə dəyişdirin.
+
+// let styleElement = document.querySelector("h1");
+// styleElement.remove();
+// let newElement = document.createElement("h1");
+// newElement.innerText = "-a";
+// document.body.insertAdjacentElement("beforebegin", newElement);
+//
+// let styleElement1 = document.querySelectorAll(".primary");
+// styleElement1.style.color = '#ebebeb'
+
+//Task-04
+//HTML-kodu verilib. Siyahının primary klasına sahib olan bütün elementlərini konsola çıxarın.
+
+// let styleElement = document.querySelector("ul");
+// console.log(styleElement.innerHTML);
+// console.log(styleElement.innerText);
+// console.log(styleElement.textContent);
+
+//Task-05
+//HTML-kod verilmişdir. Yalnız JS kodundan istifadə edərək sinifləri dəyişdirin:
+//- bütün siyahı elementlərinə item sinifi əlavə edin;
+//- siyahıda primary sinfi olmayan elementlərə secondary sinfi əlavə edin.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // "beforebegin": Before the target element itself.
 // "afterbegin": Just inside the target element, before its first child.
 // "beforeend": Just inside the target element, after its last child.
 // "afterend": After the target element itself.
-
-// let secondItem = document.querySelector(".menu li:first-child")
-// secondItem.remove()
-
-// console.log(h2elem1)
-// h2elem.textContent = "good morning"
-// console.log(h2elem.innerHTML);
-// console.log(h2elem.innerText);
-// console.log(h2elem.textContent);
